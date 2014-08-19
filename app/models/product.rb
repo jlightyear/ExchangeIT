06b125c5b2aa8d91c63a3daada60f800e10677c4
jlightyear <joseluis.estrach@gmail.com>
2014-08-19 12:36:00 +0200

    Added styles to product forms new and edit, get different size image in paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
 class Product < ActiveRecord::Base
-	has_attached_file :image
+	has_attached_file :image,
+	:styles => {
+  	:thumb    => ['160>',  :jpg, :quality => 50],
+  	:preview  => ['x150',  :jpg, :quality => 50],
+  	:large    => ['480>',      :jpg, :quality => 60],
+  	:retina   => ['1200>',     :jpg, :quality => 30]
+	},
+	:convert_options => {
+  	:thumb    => '-set colorspace sRGB -strip',
+  	:preview  => '-set colorspace sRGB -strip',
+  	:large    => '-set colorspace sRGB -strip',
+  	:retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+	}
 end
