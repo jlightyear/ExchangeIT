9365815a1479e666c304fa57be7b97aae60a99ab
jlightyear <joseluis.estrach@gmail.com>
2014-10-01 19:02:50 +0200

    Added gem pundit and policies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :products, dependent: :destroy
+  has_many :products, dependent: :destroy
   has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
